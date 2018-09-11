9b714221a5011483f58cf6de05eeadaf518efa61
petersonfreitas <sammael.tecladista@gmail.com>
2018-09-11 09:32:22 -0300

    Metodos de lista

--- class/Usuario.php
+++ class/Usuario.php
@@ -72,6 +72,52 @@ class Usuario {
 
 	}
 
+	public static function getList() {
+
+		$sql = new Sql();
+
+		return $sql->select("SELECT * FROM tb_usuarios ORDER BY deslogin");
+	}
+
+	public static function search($login) {
+
+		$sql = new SQL();
+
+		return $sql->select("SELECT * FROM tb_usuarios WHERE deslogin LIKE :SEARCH ORDER BY deslogin", array(
+			':SEARCH'=>"%".$login."%")
+		);
+		
+	}
+
+	// Comparar se usuário e senha é TRUE e carrega informações
+	public function valid($login, $password) {
+
+		$sql = new Sql();
+
+		$resultado = $sql->select("SELECT * FROM tb_usuarios where deslogin=:LOGIN AND dessenha=:PASSWORD", array(
+			":LOGIN"=>$login,
+			":PASSWORD"=>$password)
+		);
+
+		// Testa se há retorno de valor
+		//if (isset($resultado[0] > 0)){ #...... }
+		if (count($resultado) > 0) {
+
+			// Se verdadeiro há um registro na posição 0
+			$row = $resultado[0];
+
+			// Passando o resultado para dentro dos SETers
+			$this->setIdusuario($row['idusuario']);
+			$this->setDeslogin($row['deslogin']);
+			$this->setDessenha($row['dessenha']);
+			//formatando a data do banco de dados para nosso pais
+			$this->setDtcadastro(new DateTime($row['dtcadastro'])); 
+		} else {
+
+			throw new Exception ("Login e/ou senha inválidos");
+		} 
+	}
+
 	public function __toString() {
 
 		return json_encode(array(
--- index.php
+++ index.php
@@ -10,10 +10,40 @@ $user = $sql->select("SELECT * FROM tb_usuarios");
 echo json_encode($user);
 */
 
+////////////////////////////////////////
+
+/* Chama um usuário
 $user = new Usuario();
 
 $user->loadByid(1);
 
 echo $user;
+*/
+
+////////////////////////////////////////
+
+// Chama todos usuários em lista
+//$lista = Usuario::getList();
+
+//echo json_encode($lista);
+
+////////////////////////////////////////
+
+// Pesquisa todos usuários por palavras chaves
+//$pesq = Usuario::search("us");
+
+//echo json_encode($pesq);
+
+////////////////////////////////////////
+
+// Valida login e senha
+$login = new Usuario();
+
+$login->valid("Selma","@#$!");
+
+echo $login;
+
+//echo json_encode($pesq);
+
 
 ?>
